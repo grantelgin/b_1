6cb4e3339dad84ed10ef33e87f6f351b7fc6c628
grantelgin <grantelgin@gmail.com>
2013-06-16 21:28:43 -0400

    Set timezone set db

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'        => 'mysql:host=127.0.0.1;dbname=grantelg_bureaucracy',
 			'username'   => 'root',
 			'password'   => 'root',
 		),
---
+++ fuel/app/logs/2013/06/17.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2013-06-17 01:25:48 --> Error - date_default_timezone_get(): It is not safe to rely on the system's timezone settings. You are *required* to use the date.timezone setting or the date_default_timezone_set() function. In case you used any of those methods and you are still getting this warning, you most likely misspelled the timezone identifier. We selected 'America/New_York' for 'EDT/-4.0/DST' instead in /Users/grantelgin/Sites/b1/fuel/core/classes/fuel.php on line 157
